e062f9a8e0dcbb83f804f459107dd9b683a0d03f
Richard Oliver <richard@rholiver.co.uk>
2012-09-24 20:05:09 +0100

    Added client

---
+++ Jarvis.Client/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Jarvis.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DirectoryInfo _dir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void SelectResults() {
+            var text = input.Text;
+
+            Task.Factory.StartNew(() => _dir.EnumerateFiles(text + "*"))
+                .ContinueWith(files => {
+                    var results = files.Result.Select(f => new {f.Name}).ToArray();
+                    Results.ItemsSource = results;
+                },
+                              TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void Input_OnKeyUp(object sender, KeyEventArgs e) {
+            SelectResults();
+        }
+    }
+}
