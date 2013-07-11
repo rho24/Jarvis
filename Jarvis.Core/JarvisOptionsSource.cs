c56a5805dc3e8177fe733b28c3fc58489e14e6b7
Richard Oliver <richard@rholiver.co.uk>
2013-07-11 21:42:06 +0100

    Start of jarvis options

---
+++ Jarvis.Core/JarvisOptionsSource.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jarvis.Core
+{
+    public class JarvisOptionsSource : ISource
+    {
+        public string Description {
+            get { return "Jarvis options"; }
+        }
+
+        public IEnumerable<IOption> GetOptions(string term) {
+            if("jarvis".Contains(term.ToLowerInvariant()))
+                return new[] { new JarvisOption() };
+
+            return Enumerable.Empty<IOption>();
+        }
+
+        public class JarvisOption : IOption
+        {
+            public string Name
+            {
+                get { return "Jarvis"; }
+            }
+        }
+    }
+
+}
